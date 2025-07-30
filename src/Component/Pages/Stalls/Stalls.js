c65e42c345ef58a4089deec3517370163c6a0b5e
madhu-famelyn <madhu@famelyn.com>
2025-07-30 13:21:01 +0530

    testing commit 12:55

--- src/Component/Pages/Stalls/Stalls.js
+++ src/Component/Pages/Stalls/Stalls.js
@@ -48,11 +48,12 @@ export default function Stall() {
                 onClick={() => handleStallClick(stall.id)}
               >
                 <div className="stall-card">
-                  <img
-                    src={`https://fliplyn-api.onrender.com/${stall.image_url}`}
-                    alt={stall.name}
-                    className="stall-image"
-                  />
+            <img
+              src={`https://fliplyn-api.onrender.com${stall.image_url}`}  // ✅ removed extra slash
+              alt={stall.name}
+              className="stall-image"
+            />
+
                   <div className="view-menu-overlay">View Menu</div>
                 </div>
               </div>
