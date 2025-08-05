4e05693b8313d8d49642cd02312d047afc52fd42
madhu-famelyn <madhu@famelyn.com>
2025-08-05 17:06:31 +0530

    8/5/2025 3:22

--- src/Component/Pages/Header/LogoHeader.js
+++ src/Component/Pages/Header/LogoHeader.js
@@ -1,11 +1,11 @@
-// src/components/Header/LogoHeader.js
-import React from 'react';
-import './LogoHeader.css';
+// // src/components/Header/LogoHeader.js
+// import React from 'react';
+// import './LogoHeader.css';
 
-export default function LogoHeader() {
-  return (
-    <div className="signin-header-container">
-      <header className="signin-header">Fliplyne</header>
-    </div>
-  );
-}
+// export default function LogoHeader() {
+//   return (
+//     <div className="signin-header-container">
+//       <header className="signin-header">Fliplyne</header>
+//     </div>
+//   );
+// }
